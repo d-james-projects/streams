a3771726f1504d41a12125da703f95b463f93e48
d-james-projects <you@example.com>
2019-07-12 23:18:44 +0100

    examples

--- python/simpletwitter.py
+++ python/simpletwitter.py
@@ -1,21 +1,10 @@
-from streamsx.topology.topology import Topology
-import streamsx.topology.context
-import json
 import time
-from streamsx.topology import schema
 from pubnub.callbacks import SubscribeCallback
 from pubnub.enums import PNStatusCategory
 from pubnub.pnconfiguration import PNConfiguration
 from pubnub.pubnub import PubNub
-import time
-#import json
 import re
-
-output = []
-pnconfig = PNConfiguration()
-pnconfig.subscribe_key = 'sub-c-78806dd4-42a6-11e4-aed8-02ee2ddab7fe'
- 
-pubnub = PubNub(pnconfig)  
+import sys
 
 class MySubscribeCallback(SubscribeCallback):
     def presence(self, pubnub, presence):
@@ -25,32 +14,29 @@ class MySubscribeCallback(SubscribeCallback):
         pass
 
     def message(self, pubnub, message):
-        global output
-        values = {}
-        values['text'] = re.sub(r'[^\x00-\x7f]',r'',message.message['text'][0:100])
-        # write record to a list
-        output.append(values)
-
-def GetPublished():
-  while True:
-    # start the capture of published data
-    global output
-    output.clear()
-    yield iter(output)
+        value = re.sub(r'[^\x00-\x7f]',r'',message.message['text'][0:100])
+        # write record
+        print('\"{}\"'.format(value))
 
 def main():
-    global pubnub
+    pnconfig = PNConfiguration()
+    pnconfig.subscribe_key = 'sub-c-78806dd4-42a6-11e4-aed8-02ee2ddab7fe'
+ 
+    pubnub = PubNub(pnconfig)  
     my_listener = MySubscribeCallback()
     pubnub.add_listener(my_listener)    
-    pubnub.subscribe().channels('pubnub-twitter').execute()    
-    topo = Topology("simple_twitter")
-    topo.add_pip_package('pubnub')
-    Data = topo.source(GetPublished())
 
-    Data.sink(print)
-
-    streamsx.topology.context.submit("STANDALONE", topo)
+    while True:
+      try:
+        pubnub.subscribe().channels('pubnub-twitter').execute()    
+        time.sleep(1)
+        pubnub.unsubscribe_all()
+        time.sleep(1)
+      except KeyboardInterrupt:
+        pubnub.remove_listener(my_listener)
+        return sys.exc_info()
 
 if __name__ == '__main__':
-    main()
+    exc = main()
+    sys.exit(0)
 
